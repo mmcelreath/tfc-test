8addeb3053843b7474fd8bba8f0b30e593673f5a
Matt McElreath <matt.mcelreath@gmail.com>
2022-07-21 09:19:26 -0400

    Uncomment main

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# resource "null_resource" "null_resource_simple" {
-#   count = 10
-#   provisioner "local-exec" {
-#     command = "echo 'Test ${count.index}'"
-#   }
-# }
+resource "null_resource" "null_resource_simple" {
+  count = 10
+  provisioner "local-exec" {
+    command = "echo 'Test ${count.index}'"
+  }
+}
