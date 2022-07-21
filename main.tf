e810d8a17443d119202b64fe40ee874f56bfac31
Matt McElreath <matt.mcelreath@gmail.com>
2022-07-21 12:52:54 -0400

    Added Test 2

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ resource "null_resource" "tfc_test" {
     command = "echo 'Test ${count.index}'"
   }
 }
+
+resource "null_resource" "tfc_test_2" {
+  count = 10
+  provisioner "local-exec" {
+    command = "echo 'Test ${count.index}'"
+  }
+}
