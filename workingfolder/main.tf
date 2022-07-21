d27ff65cdb780bed5b32d1b83ca678c45b39e9ee
Matt McElreath <matt.mcelreath@gmail.com>
2022-07-21 09:26:15 -0400

    changed name

--- workingfolder/main.tf
+++ workingfolder/main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "null_resource_simple" {
+resource "null_resource" "null_resource_workingfolder" {
   provisioner "local-exec" {
     command = "echo 'Working Folder Test!'"
   }
